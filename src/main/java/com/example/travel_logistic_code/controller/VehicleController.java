b8901a76c5754c77dbc6b60d11bc40027fc023c0
DidierSML <didiersmaldonad@gmail.com>
2025-01-08 14:21:55 -0500

    Working on VehicleController Class

--- src/main/java/com/example/travel_logistic_code/controller/VehicleController.java
+++ src/main/java/com/example/travel_logistic_code/controller/VehicleController.java
@@ -1,4 +1,24 @@
 package com.example.travel_logistic_code.controller;
 
+import com.example.travel_logistic_code.dto.request.VehicleRequestDTO;
+import com.example.travel_logistic_code.dto.response.VehicleResponseDTO;
+import com.example.travel_logistic_code.service.VehicleService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vehicle")
+@AllArgsConstructor
 public class VehicleController {
+
+    private final VehicleService vehicleService;
+
+    public VehicleResponseDTO save (@Valid VehicleRequestDTO vehicleRequestDTO){
+
+        return vehicleService.save(vehicleRequestDTO);
+    }
+
+
 }
