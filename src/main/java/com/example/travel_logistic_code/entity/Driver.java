f1a9500e7b23f8f8b06d8556b7d52e11ba899e7b
DidierSML <didiersmaldonad@gmail.com>
2025-01-19 10:32:01 -0500

    Working on Driver entity -Extension

---
+++ src/main/java/com/example/travel_logistic_code/entity/Driver.java
@@ -0,0 +1,18 @@
+package com.example.travel_logistic_code.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Driver extends User {
+
+    private String licenseNumber;
+    private LocalDate licenseExpiryDate;
+
+    @OneToMany(mappedBy = "driver")
+    private List<Trip> trips = new ArrayList<>();
+}
