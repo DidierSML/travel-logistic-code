61f0be553bbeb8c1d896a0ddf85894c9087b735d
DidierSML <didiersmaldonad@gmail.com>
2025-01-19 10:38:43 -0500

    Working on Trip entity

--- src/main/java/com/example/travel_logistic_code/entity/Trip.java
+++ src/main/java/com/example/travel_logistic_code/entity/Trip.java
@@ -1,4 +1,23 @@
 package com.example.travel_logistic_code.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
 public class Trip {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate tripDate;
+    private Double cost;
+
+    //private Client client;
+
+    //private VehicleN vehicle;
 }
